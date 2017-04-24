8827d0f8a8126d2dc950103f5b56299bd4bcb39c
kerri alison <kerrivarner@gmail.com>
2017-04-24 09:26:28 -0400

    Update .js

--- .js
+++ .js
@@ -3,35 +3,25 @@ function getSleepHours(day) {
   return Number(hours);
 }
 function getActualSleepHours() {
-    return (function() {
-        var days = [ 'Monday', 'Tuesday', 'Wednesday',
-            'Thursday', 'Friday', 'Saturday', 'Sunday' ];
-        var hours = 0, day, i = 0;
-        for ( ; i < 7; ) {
-            day = days[i++];
-            hours += getSleepHours(day);
-        }
-        return hours;
-    })();
+  return getSleepHours('Monday') + getSleepHours('Tuesday') + getSleepHours('Wednesday') + getSleepHours('Thursday') + getSleepHours('Friday') + getSleepHours('Saturday') + getSleepHours('Sunday');
 }
-
-
 function getIdealSleepHours(){
   var idealHours = prompt('How much sleep would you prefer per night?');
   return Number(idealHours) * 7;
 }
-function calculateSleepDebt(day) {
+function calculateSleepDebt() {
   var actualSleepHours = getActualSleepHours();
   var idealSleepHours = getIdealSleepHours();
-}
+
 if(actualSleepHours === idealSleepHours){
-  console.log('You\'re on track! You got ' + actualSleepHours + 'just like you wanted!');
+  console.log('You\'re on track! You got ' + (actualSleepHours) + 'just like you wanted!');
 }
 else if (actualSleepHours > idealSleepHours){
-  console.log('You should be feeling nice and rested! You got ' (actualSleepHours - idealSleepHours));
+  console.log('You should be feeling nice and rested! You got ' + (actualSleepHours - idealSleepHours) + 'hours sleep');
 }
 else if(actualSleepHours < idealSleepHours){
   console.log('You\'d better turn in early tonight! You got ' + (idealSleepHours - actualSleepHours));
 }
-getSleepHours();
-getSleepDebt();
+}
+
+calculateSleepDebt();
